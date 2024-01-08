2631c12e30156d4b55fe9edbd8474b23319bdd6e
kdtTetz <xenosign@naver.com>
2024-01-09 01:33:28 +0900

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,23 @@
-import { useQuery } from '@tanstack/react-query';
-import { getPosts } from '../api';
+import { useQuery } from "@tanstack/react-query";
+import { getPosts } from "../api";
 
 function HomePage() {
-  const result = useQuery({ queryKey: ['posts'], queryFn: getPosts });
+  const result = useQuery({ queryKey: ["posts"], queryFn: getPosts });
 
   let posts = [];
 
-  
-
   if (result.data) posts = result.data.results;
 
-  console.log(posts)
-  
+  console.log(posts);
 
-  return <div>
-    <h1>홈페이지</h1>
-    {posts.map((post) => <p key={post.id}>{post.content}</p>)}
-  </div>;
+  return (
+    <div>
+      <h1>홈페이지</h1>
+      {posts.map((post) => (
+        <p key={post.id}>{post.content}</p>
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
